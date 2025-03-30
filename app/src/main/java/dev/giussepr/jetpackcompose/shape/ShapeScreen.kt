f3a7e2bc75d3319b3a21a4bd1c755c03e280b058
giussepr <giussepr@gmail.com>
2025-03-30 13:43:48 -0500

    Create a star using Canvas

--- app/src/main/java/dev/giussepr/jetpackcompose/shape/ShapeScreen.kt
+++ app/src/main/java/dev/giussepr/jetpackcompose/shape/ShapeScreen.kt
@@ -21,15 +21,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.PointMode
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import kotlin.math.cos
 import kotlin.math.sin
@@ -37,6 +33,28 @@ import kotlin.math.sin
 
 @Composable
 fun ShapeScreen(modifier: Modifier = Modifier) {
+    var numberOfPoints by remember { mutableIntStateOf(1) }
+    var numberOfSteps by remember { mutableIntStateOf(1) }
+    ShapeScreenContent(
+        numberOfPoints = numberOfPoints,
+        numberOfSteps = numberOfSteps,
+        onNumberOfPointsChange = {
+            numberOfPoints = it
+        },
+        onNumberOfStepsChange = {
+            numberOfSteps = it
+        })
+}
+
+@Composable
+private fun ShapeScreenContent(
+    modifier: Modifier = Modifier,
+    numberOfPoints: Int,
+    numberOfSteps: Int,
+    onNumberOfPointsChange: (Int) -> Unit,
+    onNumberOfStepsChange: (Int) -> Unit,
+) {
+    var starPath = Path()
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -45,153 +63,116 @@ fun ShapeScreen(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
     ) {
-        var numberOfPoints by remember { mutableIntStateOf(1) }
-        ShapeScreenContent(numberOfPoints, onNumberOfPointsChange = {
-            numberOfPoints = it
-        })
-        /*Image(
-            modifier = Modifier
-                .size(200.dp)
-                .clip(CircleShape),
-            contentScale = ContentScale.Crop,
-            painter = painterResource(R.drawable.living_room),
-            contentDescription = null
-        )*/
-    }
-}
-
-@Composable
-private fun ShapeScreenContent(numberOfPoints: Int, onNumberOfPointsChange: (Int) -> Unit) {
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        OutlinedTextField(
-            modifier = Modifier
-                .weight(1f),
-            value = numberOfPoints.toString(),
-            label = {
-                Text("Number of points")
-            },
-            onValueChange = {
-                val regex = "-?[0-9]+(\\.[0-9]+)?".toRegex()
-                if (it.matches(regex)) {
-                    onNumberOfPointsChange(it.toIntOrNull() ?: 0)
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            OutlinedTextField(
+                modifier = Modifier
+                    .weight(1f),
+                value = numberOfPoints.toString(),
+                label = {
+                    Text("Number of points")
+                },
+                onValueChange = {
+                    val regex = "-?[0-9]+(\\.[0-9]+)?".toRegex()
+                    if (it.matches(regex)) {
+                        onNumberOfPointsChange(it.toIntOrNull() ?: 0)
+                    }
                 }
-            }
-        )
-        OutlinedTextField(
-            modifier = Modifier
-                .weight(1f),
-            value = numberOfPoints.toString(),
-            label = {
-                Text("Number of points")
-            },
-            onValueChange = {
-                val regex = "-?[0-9]+(\\.[0-9]+)?".toRegex()
-                if (it.matches(regex)) {
-                    onNumberOfPointsChange(it.toIntOrNull() ?: 0)
+            )
+            OutlinedTextField(
+                modifier = Modifier
+                    .weight(1f),
+                value = numberOfSteps.toString(),
+                label = {
+                    Text("Number of steps")
+                },
+                onValueChange = {
+                    val regex = "-?[0-9]+(\\.[0-9]+)?".toRegex()
+                    if (it.matches(regex)) {
+                        onNumberOfStepsChange(it.toIntOrNull() ?: 0)
+                    }
                 }
-            }
-        )
-    }
-    if (numberOfPoints > 0) {
-        Canvas(modifier = Modifier.size(300.dp)) {
-            val canvasWidth = size.width
-            val canvasHeight = size.height
-            drawRect(
-                color = Color.Blue,
-                size = Size(canvasWidth, canvasHeight),
-                style = Stroke(2f)
             )
-            drawCircle(
-                color = Color.Blue,
-                radius = canvasWidth / 2,
-                style = Stroke(2f),
-            )
-            // Center point
-            drawPoints(
-                points = listOf(
-                    Offset(canvasWidth / 2, canvasHeight / 2),
-                ),
-                pointMode = PointMode.Points,
-                color = Color.Red,
-                strokeWidth = 20f,
-                cap = StrokeCap.Round
-            )
-
-            // Draw the numberOfPoints in the circle
-            val angle = 360 / numberOfPoints
-            val angles = mutableListOf<Int>()
-            for (i in 0..<360 step angle) {
-                angles.add(i)
-            }
-            // The center of out circle is the middle of our canvas
-            val circleX = canvasWidth / 2
-            val circleY = canvasHeight / 2
-            val radius = canvasWidth / 2
-            val startPoints = mutableListOf<Offset>()
-            angles.forEach { angle ->
-                // we need to convert the angle from degrees to radians
-                // Subtract 90 to start in superior part of the circle
-                val radians = Math.toRadians(angle.toDouble() - 90)
-                // Find x using trigonometry, x = r * cos(angle)
-                val x: Float = radius * cos(radians).toFloat()
-                // Fin y using trigonometry, y = r * sen(angle)
-                val y: Float = radius * sin(radians).toFloat()
-                val startPoint = Offset(x + circleX, y + circleY)
-                startPoints.add(startPoint)
+        }
+        if (numberOfPoints > 0) {
+            Canvas(modifier = Modifier.size(300.dp)) {
+                val canvasWidth = size.width
+                val canvasHeight = size.height
+                drawRect(
+                    color = Color.Blue,
+                    size = Size(canvasWidth, canvasHeight),
+                    style = Stroke(2f)
+                )
+                drawCircle(
+                    color = Color.Blue,
+                    radius = canvasWidth / 2,
+                    style = Stroke(2f),
+                )
+                // Center point
                 drawPoints(
                     points = listOf(
-                        startPoint
+                        Offset(canvasWidth / 2, canvasHeight / 2),
                     ),
                     pointMode = PointMode.Points,
                     color = Color.Red,
                     strokeWidth = 20f,
                     cap = StrokeCap.Round
                 )
-            }
-            // Draw the polygon
-            val polygonPath = Path().apply {
-                moveTo(startPoints.first().x, startPoints.first().y)
-                for (i in 0 until startPoints.size) {
-                    lineTo(startPoints[i].x, startPoints[i].y)
-                }
-                close()
-            }
-            drawPath(path = polygonPath, color = Color.Red, style = Stroke(2f))
-            // Draw the star
-            val starOrder = List(startPoints.size) { i -> (i * 2) % startPoints.size }
-            val starPath = Path().apply {
-                moveTo(startPoints[starOrder[0]].x, startPoints[starOrder[0]].y)
-                for (i in 1 until starOrder.size) {
-                    lineTo(startPoints[starOrder[i]].x, startPoints[starOrder[i]].y)
-                }
-                close()
-            }
-            drawPath(starPath, color = Color.Blue, style = Stroke(width = 4f))
-        }
-    }
-}
 
-data object PentagonShape : Shape {
-    override fun createOutline(
-        size: Size,
-        layoutDirection: LayoutDirection,
-        density: Density
-    ): Outline {
-        val pointSpace = size.width / 5f
-        return Outline.Generic(
-            path = Path().apply {
-                moveTo(
-                    x = size.width / 2,
-                    y = 0f
-                )
-                lineTo(
-                    x = 0f,
-                    y = 0f
-                )
+                // Draw the numberOfPoints in the circle
+                val angle = 360 / numberOfPoints
+                val angles = mutableListOf<Int>()
+                for (i in 0..<360 step angle) {
+                    angles.add(i)
+                }
+                // The center of out circle is the middle of our canvas
+                val circleX = canvasWidth / 2
+                val circleY = canvasHeight / 2
+                val radius = canvasWidth / 2
+                val startPoints = mutableListOf<Offset>()
+                angles.forEach { angle ->
+                    // we need to convert the angle from degrees to radians
+                    // Subtract 90 to start in superior part of the circle
+                    val radians = Math.toRadians(angle.toDouble() - 90)
+                    // Find x using trigonometry, x = r * cos(angle)
+                    val x: Float = radius * cos(radians).toFloat()
+                    // Fin y using trigonometry, y = r * sen(angle)
+                    val y: Float = radius * sin(radians).toFloat()
+                    val startPoint = Offset(x + circleX, y + circleY)
+                    startPoints.add(startPoint)
+                    drawPoints(
+                        points = listOf(
+                            startPoint
+                        ),
+                        pointMode = PointMode.Points,
+                        color = Color.Red,
+                        strokeWidth = 20f,
+                        cap = StrokeCap.Round
+                    )
+                }
+                // Draw the polygon
+                val polygonPath = Path().apply {
+                    moveTo(startPoints.first().x, startPoints.first().y)
+                    for (i in 0 until startPoints.size) {
+                        lineTo(startPoints[i].x, startPoints[i].y)
+                    }
+                    close()
+                }
+                drawPath(path = polygonPath, color = Color.Red, style = Stroke(2f))
+                // Draw the star
+                val starOrder =
+                    List(startPoints.size) { i -> (i * numberOfSteps) % startPoints.size }
+                starPath = Path().apply {
+                    moveTo(startPoints[starOrder[0]].x, startPoints[starOrder[0]].y)
+                    for (i in 1 until starOrder.size) {
+                        lineTo(startPoints[starOrder[i]].x, startPoints[starOrder[i]].y)
+                    }
+                    close()
+                }
+                drawPath(starPath, color = Color.Blue, style = Stroke(width = 4f))
             }
-        )
+        }
     }
 }
 
@@ -199,8 +180,11 @@ data object PentagonShape : Shape {
 @Composable
 private fun ShapeScreenPreview() {
     MaterialTheme {
-        Column {
-            ShapeScreenContent(6) { }
-        }
+        ShapeScreenContent(
+            numberOfPoints = 5,
+            numberOfSteps = 2,
+            onNumberOfPointsChange = {},
+            onNumberOfStepsChange = {}
+        )
     }
 }
